2ac606e0447b718543be69003478baeef313ccc8
mmk31585 <mmkabiri31585@gmail.com>
2026-09-23 09:30:40 +0330

    refactor: change enventto result

--- internal/message/update.go
+++ internal/message/update.go
@@ -4,15 +4,8 @@ type UpdateCommand struct {
 	OperationID string `json:"operation_id"`
 	Service     string `json:"service"`
 }
-type OperationStatus string
 
-const (
-	StatusPending    OperationStatus = "PENDING"
-	StatusDispatched OperationStatus = "DISPATCHED"
-	StatusRunning    OperationStatus = "RUNNING"
-	StatusSucceeded  OperationStatus = "SUCCEEDED"
-)
-type OperationEvent struct {
-	OperationID string          `json:"operation_id"`
-	Status      OperationStatus `json:"status"`
+type UpdateResult struct {
+	OperationID string `json:"operation_id"`
+	Status      string `json:"status"`
 }
