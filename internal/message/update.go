91e9653680d34d7389ee551c73f3bb4decac46c9
mmk31585 <mmkabiri31585@gmail.com>
2026-09-23 15:23:29 +0330

    feat: file fields

--- internal/message/update.go
+++ internal/message/update.go
@@ -3,9 +3,15 @@ package message
 type UpdateCommand struct {
 	OperationID string `json:"operation_id"`
 	Service     string `json:"service"`
+
+	FileURL    string `json:"file_url"`
+	FileName   string `json:"file_name"`
+	FileSize   int64  `json:"file_size"`
+	FileSHA256 string `json:"file_sha256"`
 }
 
 type UpdateResult struct {
 	OperationID string `json:"operation_id"`
 	Status      string `json:"status"`
+	Error       string `json:"error,omitempty"`
 }
