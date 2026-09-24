d48f9f036bac5fa565f575e33436befe00acc967
mmk31585 <mmkabiri31585@gmail.com>
2026-09-24 11:51:58 +0330

    feat: node instance id for command

--- internal/message/update.go
+++ internal/message/update.go
@@ -1,8 +1,9 @@
 package message
 
 type UpdateCommand struct {
-	OperationID string `json:"operation_id"`
-	Service     string `json:"service"`
+	OperationID  string `json:"operation_id"`
+	Service      string `json:"service"`
+	NodeInstance string `json:"node_instance"`
 
 	FileURL    string `json:"file_url"`
 	FileName   string `json:"file_name"`
