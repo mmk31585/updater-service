6a1c7e957f0e7785efaafcd1a3b480c6d8d25a2d
mmk31585 <mmkabiri31585@gmail.com>
2026-09-24 11:57:09 +0330

    feat: subjects prefix

---
+++ internal/subjects/subjects.go
@@ -0,0 +1,9 @@
+package subjects
+
+const (
+	CommandPrefix = "update.command."
+	Result        = "update.result"
+	NodeRegister  = "node.register"
+	NodeHeartbeat = "node.heartbeat"
+	NodeGoodbye   = "node.goodbye"
+)
